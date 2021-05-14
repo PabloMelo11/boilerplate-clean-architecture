feff2163b0689a15557db6d1c38f74162ee4e84e
PabloMelo <pablomelodev@gmail.com>
2021-05-13 23:00:31 -0300

    test: testing user password value object

---
+++ tests/unitary/entities/password.spec.ts
@@ -0,0 +1,21 @@
+import { Password } from '@/domain/entities/user/password';
+
+describe('User password value object', () => {
+  it('should accept valid password', () => {
+    const passwordOrError = Password.create('123456');
+
+    expect(passwordOrError.isRight()).toBeTruthy();
+  });
+
+  it('should reject password with less than 6 characters', () => {
+    const passwordOrError = Password.create('12345');
+
+    expect(passwordOrError.isLeft()).toBeTruthy();
+  });
+
+  it('should reject password with more than 255 characters', () => {
+    const passwordOrError = Password.create('1'.repeat(260));
+
+    expect(passwordOrError.isLeft()).toBeTruthy();
+  });
+});
